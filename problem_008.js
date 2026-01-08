b95d9ac098f81a06cd72931f21f6bef70d04703b
whahidul12 <mdislamwhahid@gmail.com>
2026-01-08 17:25:39 +0600

    refactor: rename parameter in capitalize_words function for clarity

--- problem_008.js
+++ problem_008.js
@@ -1,5 +1,5 @@
-function capitalize_words(str) {
-    let words = str.split(" ");
+function capitalize_words(mySentence) {
+    let words = mySentence.split(" ");
     for (let i = 0; i < words.length; i++) {
         words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
     }
